d01d28521e0a94b8f60da2b814323d3d510937c6
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2021-09-09 23:01:02 +0300

    feat: Updated Myteam

--- johnlepikhin/packages/myteam.scm
+++ johnlepikhin/packages/myteam.scm
@@ -82,6 +82,8 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
      `(#:install-plan
        `(("myteam" "bin/myteam")
          ("lib" "lib")
+         ("libexec" "libexec")
+         ("resources" "resources")
          ("plugins" "plugins"))
        #:phases
        (modify-phases
@@ -94,14 +96,27 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
                  (ncurses-lib
                   (string-append (assoc-ref inputs "ncurses") "/lib"))
                  (patchelf (string-append (assoc-ref inputs "patchelf") "/bin/patchelf"))
-                 (binary (string-append out "/bin/myteam"))
-                 (libs (string-append out "/lib/lib*.so*"
-                                      " " out "/plugins/*/lib*.so*"))
+                 (binaries (string-append out "/bin/myteam"
+                                          " " out "/libexec/*"))
+                 (libs (string-append out "/lib/lib*.so*"))
+                 (plugins (string-append out "/plugins/*/lib*.so*"))
                  (dynamic-linker (string-append (assoc-ref inputs "libc") ,(glibc-dynamic-linker)))
                  (nss (string-append (assoc-ref inputs "nss") "/lib/nss")))
-            (system (string-append patchelf " --set-rpath \"" out "/lib:" nss ":" out "/plugins:$LIBRARY_PATH\" --set-interpreter " dynamic-linker " " binary))
-            ;; (system (string-append "rm -rf " out "/lib " out "/plugins"))
-            (system (string-append patchelf " --set-rpath \"" out "/lib:" nss ":" out "/plugins:$LIBRARY_PATH\" " libs))
+            (system
+             (string-append
+              patchelf
+              " --set-rpath \"" out "/lib:" nss ":" out "/plugins:$LIBRARY_PATH\""
+              " --set-interpreter " dynamic-linker
+              " " binaries))
+
+            (system
+             (string-append
+              patchelf " --set-rpath \"" out "/lib:" nss ":" out "/plugins:$LIBRARY_PATH\" " libs))
+
+            ;; TODO for some reason plugins are not patched
+            (system
+             (string-append
+              patchelf " --set-rpath \"" out "/lib:" nss ":" out "/plugins:$LIBRARY_PATH\" " plugins))
 
             (symlink
              (string-append ncurses-lib "/libncursesw.so."
@@ -131,7 +146,8 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
               ("mesa" ,mesa)
               ("glib" ,glib)
               ("libxi" ,libxi)
-              ("gcc:lib" ,(canonical-package gcc) "lib")
+              ("eudev" ,eudev)
+              ;; ("gcc:lib" ,(canonical-package gcc) "lib")
               ("ncurses" ,ncurses)
               ("libxtst" ,libxtst)
               ("alsa-lib" ,alsa-lib)
@@ -139,13 +155,7 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
               ("libxinerama" ,libxinerama)))
     (license gpl3+)))
 
-(define-public myteam-10.0.8143
-  (make-myteam "10.0.8143" "0f7m08lpfwxl7kaw3fsc73d4sp9sfbl6dh4yxm8mr6px8rv3g2h9"))
+(define-public myteam-10.0.9824
+  (make-myteam "10.0.9824" "2kdkddddddk9051i05baznrawy958a4qvw753fmxalsb6dxgflq2"))
 
-(define-public myteam-10.0.8804
-  (make-myteam "10.0.8804" "16igq814qw63j8p7lzsj2376xf2hz1gla9yy2c9aic6fy02wi8n3"))
-
-(define-public myteam-10.0.9663
-  (make-myteam "10.0.9663" "1kpksd3icgk9051i05baznrawy958a4qvw753fmxalsb6dxgflqv"))
-
-(define-public myteam myteam-10.0.9663)
+(define-public myteam myteam-10.0.9824)
