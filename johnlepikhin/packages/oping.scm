0ee6b7cf75159e35602f4f78a786834da3a05963
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2021-12-25 19:16:51 +0300

    fix: Forked liboping

--- johnlepikhin/packages/oping.scm
+++ johnlepikhin/packages/oping.scm
@@ -7,12 +7,17 @@
   #:use-module (gnu packages elf)
   #:use-module (guix build-system gnu))
 
-(define (liboping version checksum)
+(define (liboping version commit checksum)
   (package
    (name "liboping")
    (version version)
    (source (origin
-            (method url-fetch)
+             (method git-fetch)
+             (uri (git-reference
+                   (url "https://github.com/johnlepikhin/liboping")
+                    (commit commit)))
+
+             (method url-fetch)
             (uri (string-append "https://noping.cc/files/liboping-" version ".tar.bz2"))
             (sha256 (base32 checksum))))
    (build-system gnu-build-system)
@@ -23,7 +28,7 @@
    (home-page "https://noping.cc")
    (license lgpl2.1)))
 
-(define-public liboping-1.10.0
-  (liboping "1.10.0" "1n2wkmvw6n80ybdwkjq8ka43z2x8mvxq49byv61b52iyz69slf7b"))
+(define-public liboping-1.10.1
+  (liboping "1.10.1" "7a6e4795c5fb1a98624c3a1c922da87b44476200" "1n2wkmvw6n80ybdwkjq8ka43z2x8mvxq49byv61b52iyz69slf7b"))
 
-(define-public liboping liboping-1.10.0)
+(define-public liboping liboping-1.10.1)
