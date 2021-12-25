3d8573a568aacd8500b847485b52372fe713e319
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2021-12-25 19:45:33 +0300

    fix: Added missing deps

--- johnlepikhin/packages/oping.scm
+++ johnlepikhin/packages/oping.scm
@@ -1,10 +1,12 @@
 
 (define-module (johnlepikhin packages oping)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages elf)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (guix git-download)
   #:use-module (guix licenses)
   #:use-module (guix packages)
-  #:use-module (guix git-download)
-  #:use-module (gnu packages perl)
-  #:use-module (gnu packages elf)
   #:use-module (guix build-system gnu))
 
 (define (liboping version commit checksum)
@@ -19,13 +21,19 @@
              (sha256 (base32 checksum))))
    (build-system gnu-build-system)
    (supported-systems '("x86_64-linux"))
-   (native-inputs `(("perl" ,perl)))
+   (native-inputs `(("perl" ,perl)
+                    ("autoconf" ,autoconf)
+                    ("libtool" ,libtool)
+                    ("pkg-config" ,pkg-config)
+                    ("automake" ,automake)))
    (synopsis "octo's ping library")
    (description "octo's ping library")
    (home-page "https://noping.cc")
    (license lgpl2.1)))
 
 (define-public liboping-1.10.1
-  (liboping "1.10.1" "7a6e4795c5fb1a98624c3a1c922da87b44476200" "1n2wkmvw6n80ybdwkjq8ka43z2x8mvxq49byv61b52iyz69slf7b"))
+  (liboping "1.10.1" "7a6e4795c5fb1a98624c3a1c922da87b44476200" "1nmhi4652izd0adrs72m038cy035snn7c8kmhrafx2zw7vwwnrgj"))
 
 (define-public liboping liboping-1.10.1)
+
+liboping
