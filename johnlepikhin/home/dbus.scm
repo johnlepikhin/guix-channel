15a3279fed261ea00ad703e4c028728dcea51e0a
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2022-12-06 17:43:48 +0300

    feat: Added dbus service

---
+++ johnlepikhin/home/dbus.scm
@@ -0,0 +1,36 @@
+;; Imported from https://git.sr.ht/~krevedkokun/guix-channel
+
+(define-module (johnlepikin home dbus)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shepherd)
+  #:use-module (guix gexp))
+
+(define (home-dbus-environment-variables-service _)
+  '(("DBUS_SESSION_BUS_ADDRESS" . "unix:path=$XDG_RUNTIME_DIR/dbus.sock")))
+
+(define (home-dbus-shepherd-service _)
+  (list
+   (shepherd-service
+    (provision '(dbus))
+    (stop  #~(make-kill-destructor))
+    (start #~(make-forkexec-constructor
+              (list #$(file-append (@@ (gnu packages glib) dbus)
+                                   "/bin/dbus-daemon")
+                    "--nofork"
+                    "--session"
+                    (string-append
+                     "--address=" "unix:path="
+                     (getenv "XDG_RUNTIME_DIR") "/dbus.sock")))))))
+
+(define-public home-dbus-service-type
+  (service-type
+   (name 'home-dbus)
+   (extensions
+    (list (service-extension
+           home-environment-variables-service-type
+           home-dbus-environment-variables-service)
+          (service-extension
+           home-shepherd-service-type
+           home-dbus-shepherd-service)))
+   (default-value #f)
+   (description "run dbus")))
