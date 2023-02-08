ebbf3bcd9c647f4615ed57fe80d09bd089583348
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2023-02-08 20:43:59 +0300

    refactor: Module cleanup, some deps were merged into main channel

--- johnlepikhin/packages/shadowplay.scm
+++ johnlepikhin/packages/shadowplay.scm
@@ -79,66 +79,6 @@
      "YAML parser which provides AST with saved tokens positions.")
     (license (list license:expat))))
 
-(define-public rust-minimal-lexical-0.2
-  (package
-    (name "rust-minimal-lexical")
-    (version "0.2.1")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (crate-uri "minimal-lexical" version))
-        (file-name (string-append name "-" version ".tar.gz"))
-        (sha256
-         (base32 "16ppc5g84aijpri4jzv14rvcnslvlpphbszc7zzp6vfkddf4qdb8"))))
-    (build-system cargo-build-system)
-    (home-page "https://github.com/Alexhuszagh/minimal-lexical")
-    (synopsis "Fast float parsing conversion routines")
-    (description "Fast float parsing conversion routines.")
-    (license (list license:expat license:asl2.0))))
-
-(define-public rust-nom-7.1
-  (package
-    (name "rust-nom")
-    (version "7.1.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "nom" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32
-         "0djc3lq5xihnwhrvkc4bj0fd58sjf632yh6hfiw545x355d3x458"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:tests? #f  ; Tests require example directory, not included in tarball.
-       #:cargo-inputs
-       (("rust-memchr" ,rust-memchr-2)
-        ("rust-minimal-lexical" ,rust-minimal-lexical-0.2)
-        ("rust-version-check" ,rust-version-check-0.9))
-       #:cargo-development-inputs
-       (("rust-criterion" ,rust-criterion-0.3)
-        ("rust-doc-comment" ,rust-doc-comment-0.3)
-        ("rust-jemallocator" ,rust-jemallocator-0.3)
-        ("rust-proptest" ,rust-proptest-1))
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'configure 'override-jemalloc
-           (lambda* (#:key inputs #:allow-other-keys)
-             (let ((jemalloc (assoc-ref inputs "jemalloc")))
-               (setenv "JEMALLOC_OVERRIDE"
-                       (string-append jemalloc "/lib/libjemalloc_pic.a")))
-             #t)))))
-    (native-inputs
-     (list jemalloc))
-    (home-page "https://github.com/Geal/nom")
-    (synopsis
-     "Byte-oriented, zero-copy, parser combinators library")
-    (description
-     "This package provides a byte-oriented, zero-copy, parser
-combinators library.")
-    (license license:expat)))
-
 (define-public rust-serde-regex-1
   (package
     (name "rust-serde-regex")
@@ -179,7 +119,7 @@ It's often useful to read regexes from configuration file.")
          ("rust-env-logger" ,rust-env-logger-0.9)
          ("rust-lazy-static" ,rust-lazy-static-1)
          ("rust-log" ,rust-log-0.4)
-         ("rust-nom" ,rust-nom-7.1)
+         ("rust-nom" ,rust-nom-7)
          ("rust-serde" ,rust-serde-1)
          ("rust-serde-json" ,rust-serde-json-1)
          ("rust-serde-yaml" ,rust-serde-yaml-0.8)
