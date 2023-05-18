43a9a18be50e1207079153e62fe9a9f4cdb98e47
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2023-05-18 10:22:59 +0300

    fix: Added missing input

--- johnlepikhin/packages/vkteams.scm
+++ johnlepikhin/packages/vkteams.scm
@@ -186,6 +186,7 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
               ("libxcomposite" ,libxcomposite)
               ("libxcursor" ,libxcursor)
               ("libxdamage" ,libxdamage)
+              ("gcc" ,gcc)
               ("libgpg-error" ,libgpg-error)
               ("glibc" ,glibc)
               ("libfontconfig" ,fontconfig)
@@ -203,7 +204,7 @@ own.  This helper makes it easier to deal with \"tar bombs\"."
                ("libxinerama" ,libxinerama)))
     (license gpl3+)))
 
-(define-public vkteams-23.02.0
-  (make-vkteams "23.02.0" "https://t.bk.ru/lQ8MG7Buvx1ugDI/vkteams.tar.xz" "0kjng9rm89b8iz574p6ykvjvdhs8w5dn85da66mdsxkp9gkcka0x"))
+(define-public vkteams-23.02
+  (make-vkteams "23.02.1" "https://t.bk.ru/lQ8MG7Buvx1ugDI/vkteams.tar.xz" "0kjng9rm89b8iz574p6ykvjvdhs8w5dn85da66mdsxkp9gkcka0x"))
 
-(define-public vkteams vkteams-23.02.0)
+(define-public vkteams vkteams-23.02)
