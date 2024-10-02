52318413ca25361ef006c7cd232fd90112001641
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2024-10-02 20:22:15 +0300

    feat: Updated apcalc

--- johnlepikhin/packages/apcalc.scm
+++ johnlepikhin/packages/apcalc.scm
@@ -24,6 +24,7 @@
   #:use-module (gnu packages readline)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages elf)
+  #:use-module (gnu packages man)
   #:use-module (gnu packages groff)
   #:use-module (guix build-system gnu)
   #:export (make-apcalc))
@@ -43,44 +44,43 @@
    (build-system gnu-build-system)
    (supported-systems '("x86_64-linux"))
    (arguments
-    `(#:parallel-build? #f
+    `(#:parallel-build? #t
       #:phases
       (modify-phases
        %standard-phases
        (delete 'configure)
+       ;; fails for some reason
+       (delete 'validate-runpath)
        (add-before
         'build 'patch-makefile
         (lambda* (#:key outputs #:allow-other-keys)
-          (map (lambda (file-path)
-                 (substitute* file-path
-                              (("^PREFIX *=.*") (string-append "PREFIX=\n"))
-                              (("^T *=.*") (string-append "T=\n"))
-                              (("^DEFAULT_LIB_INSTALL_PATH *=.*") (string-append "DEFAULT_LIB_INSTALL_PATH=" %output "/lib\n"))
-                              (("^SHELL *=.*") (string-append "SHELL=" (which "sh") "\n"))
-                              (("^BINDIR *=.*") (string-append "BINDIR=" %output "/bin\n"))
-                              (("^LIBDIR *=.*") (string-append "LIBDIR=" %output "/lib\n"))
-                              (("^MANDIR *=.*") (string-append "MANDIR=" %output "/share/man/man1\n"))
-                              (("^CATDIR *=.*") (string-append "CATDIR=" %output "/share/man/cat1\n"))
-                              (("^INCDIR *=.*") (string-append "INCDIR=" %output "/include\n"))
-                              (("^CALC_SHAREDIR *=.*") (string-append "CALC_SHAREDIR=" %output "/share/calc\n"))))
-               (list
-                "Makefile.ship"
-                "custom/Makefile"
-                "cal/Makefile"
-                "help/Makefile"
-                "cscript/Makefile"
-                "help/Makefile"))
+          (substitute* "Makefile.target"
+            (("^DEFAULT_LIB_INSTALL_PATH *=.*") (string-append "DEFAULT_LIB_INSTALL_PATH=" %output "/lib\n")))
+          (call-with-output-file "Makefile.local"
+            (lambda (port)
+              (format port "\n")
+              (format port "T=\n")
+              (format port (string-append "SHELL=" (which "sh") "\n"))
+              (format port (string-append "BINDIR=" %output "/bin\n"))
+              (format port (string-append "LIBDIR=" %output "/lib\n"))
+              (format port (string-append "MANDIR=" %output "/share/man/man1\n"))
+              (format port (string-append "CATDIR=" %output "/share/man/cat1\n"))
+              (format port (string-append "INCDIR=" %output "/include\n"))
+              (format port (string-append "CALC_SHAREDIR=" %output "/share/calc\n"))))
           #t)))))
    (inputs `(("readline" ,readline)))
    (native-inputs `(("util-linux" ,util-linux)
-                    ("groff-minimal" ,groff-minimal)))
+                    ("groff-minimal" ,groff-minimal)
+                    ("man-db" ,man-db)))
    (synopsis "C-style arbitrary precision calculator")
    (description "Calc is an interactive calculator which provides for easy large numeric calculations, but which also can be easily
 programmed for difficult or long calculations. ")
    (home-page "https://github.com/lcn2/calc")
    (license lgpl2.1)))
 
-(define-public apcalc-2.13.0.1
-  (make-apcalc "2.13.0.1" "020nfbcqk5n33qsnbxh6b7xz883qlb25j8wzx7009k5lj5424n9h"))
+(define-public apcalc-2.15.1.0
+  (make-apcalc "2.15.1.0" "1xhya6r6k9hx1dir3z2ffj237gk9ky3w23mw1bvcmmbm4k0nyvz2"))
 
-(define-public apcalc apcalc-2.13.0.1)
+(define-public apcalc apcalc-2.15.1.0)
+
+apcalc
