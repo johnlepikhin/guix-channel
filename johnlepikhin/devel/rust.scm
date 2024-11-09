52fc7abf6dd89269d2ff8e7ae393d76908ecf917
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2024-11-09 07:06:15 +0300

    fix: Added missing output "cargo"

--- johnlepikhin/devel/rust.scm
+++ johnlepikhin/devel/rust.scm
@@ -42,7 +42,8 @@
      clang
      openssl-1.1
      rust-package
-     (list rust-package "tools"))))
+     (list rust-package "tools")
+     (list rust-package "cargo"))))
 
 (define (add-env-variables config)
   `(("RUST_SRC_PATH" . ,(string-append (getenv "HOME") "/.guix-home/profile/lib/rustlib/src/rust/library"))
