779498ce61ec6be44ba961434ac7d5b738b23167
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2024-11-30 17:14:00 +0300

    feat: Added intel-cpu-throttle-fix-service

--- johnlepikhin/system/services.scm
+++ johnlepikhin/system/services.scm
@@ -160,6 +160,28 @@ host    all all ::1/128     md5"))
      (avahi-configuration
       (ipv6? #f))))))
 
+(define-public (intel-cpu-throttle-fix-service _)
+  (list (shepherd-service
+         (provision '(intel-cpu-throttle-fix))
+         (documentation "On some Intel CPUs, the CPU frequency may be throttled to 400 MHz
+after few seconds of high load. There many similar issues on the internet and
+this service fixes only one class of them, which is described here:
+https://unix.stackexchange.com/a/615343.
+
+Important note: module processor_thermal_device_pci MUST be unloaded after boot.
+Blacklisting it in modprobe is not enough.")
+         (one-shot? #t)
+         (start #~(lambda _ (system "/run/current-system/profile/bin/rmmod processor_thermal_device_pci"))))))
+
+(define intel-cpu-throttle-fix-service-type
+  (service-type
+   (name 'intel-cpu-throttle-fix)
+   (description "Fix Intel CPU throttling issue")
+   (extensions
+    (list
+     (service-extension
+      shepherd-root-service-type intel-cpu-throttle-fix-service)))))
+
 (define (public-backlight-brightness-service _)
   (list (shepherd-service
          (provision '(public-backlight-brightness))
