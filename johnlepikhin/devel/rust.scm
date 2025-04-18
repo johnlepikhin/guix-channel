10152cad9f436c2288cbe0abbf4af614d43277ee
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2025-04-18 21:08:22 +0300

    fix: [WIP] Use Rust from the main channel

--- johnlepikhin/devel/rust.scm
+++ johnlepikhin/devel/rust.scm
@@ -25,7 +25,6 @@
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:use-module (guix records)
-  #:use-module (johnlepikhin packages rust)
   #:use-module (srfi srfi-1)
   #:export (home-devel-rust-configuration
             home-devel-rust-service-type))
@@ -33,7 +32,7 @@
 (define-record-type* <home-devel-rust-configuration>
   home-devel-rust-configuration make-home-devel-rust-configuration
   home-devel-rust-configuration?
-  (package home-devel-rust-configuration-package (default rust-next))
+  (package home-devel-rust-configuration-package (default rust))
   (edition home-devel-rust-configuration-edition (default "2021")))
 
 (define (add-devel-rust-packages config)
