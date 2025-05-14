3c97f26fdd5c8bcf03e8b76e3b8c5eb05d1fa64d
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2025-05-14 18:44:21 +0300

    feat: Added package google-chrome-beta-system-egl

---
+++ johnlepikhin/packages/google-chrome.scm
@@ -0,0 +1,32 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2024 Evgenii Lepikhin <johnlepikhin@gmail.com>
+;;;
+;;; This file is not part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+(use-modules (guix packages)
+             (guix gexp)
+             (guix utils)
+             (gnu packages gl)
+             (nongnu packages chrome))
+
+(define-public google-chrome-beta-system-egl
+  (let* ((base google-chrome-beta)
+         (args (package-arguments base)))
+    (package
+      (inherit base)
+      (name "google-chrome-beta-system-egl")
+      (inputs
+       (modify-inputs (package-inputs google-chrome-beta)
+                      (prepend mesa))))))
