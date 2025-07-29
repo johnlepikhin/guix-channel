6cd77ba9b5573ce05b762843d23b33a1e62f5c6f
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2025-07-29 19:15:53 +0300

    feat: Add GPU support wrapper for Google Chrome

--- johnlepikhin/packages/google-chrome.scm
+++ johnlepikhin/packages/google-chrome.scm
@@ -17,28 +17,26 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 ;;;
 
-;; Chrome is dynamically linked against libGLX and libEGL from the libglvnd library.
-;; Libglvnd acts as a proxy/dispatcher that routes OpenGL API calls to the appropriate
-;; backend provider (such as Mesa or proprietary drivers). It selects which implementation
-;; to use at runtime based on available drivers.
+;; Chrome uses ANGLE for OpenGL ES/WebGL support and expects specific GL implementations.
+;; In Guix, Chrome fails to initialize GPU with error:
+;; "Requested GL implementation (gl=none,angle=none) not found in allowed implementations: [(gl=egl-angle,angle=default)]"
+;;
+;; This happens because:
+;; 1. Chrome's GPU process tries to initialize with wrong GL parameters
+;; 2. The Mesa drivers path is not properly set in the environment
+;; 3. VA-API drivers for hardware video decoding are not found
+;;
+;; This package fixes these issues by:
+;; 1. Adding Mesa to inputs for proper OpenGL support
+;; 2. Creating a new wrapper that sets environment variables and GPU flags
 
-;; However, in Guix, the version of libglvnd (or its integration with the rest of the stack)
-;; may be outdated or incompatible, resulting in Chrome encountering `undefined symbol` errors
-;; during library loading.
-
-;; To work around this issue, I created a custom package that adds `mesa` to the runtime
-;; dependencies. Crucially, `mesa` is prepended to the `inputs` list, so it appears first
-;; in the generated LD_LIBRARY_PATH.
-
-;; As a result, nonguix's build system (via the chromium-binary-build-system) generates a
-;; wrapper script that sets LD_LIBRARY_PATH in the correct order. This ensures that Chrome
-;; attempts to load libGLX and libEGL directly from Mesa first, avoiding conflicts or missing
-;; symbols caused by relying solely on libglvnd.
 (define-module (johnlepikhin packages google-chrome)
   #:use-module (guix packages)
   #:use-module (guix gexp)
   #:use-module (guix utils)
+  #:use-module (guix search-paths)
   #:use-module (gnu packages gl)
+  #:use-module (gnu packages video)
   #:use-module (nongnu packages chrome))
 
 (define-public google-chrome-beta-system-egl
@@ -47,4 +45,44 @@
     (name "google-chrome-beta-system-egl")
     (inputs
      (modify-inputs (package-inputs google-chrome-beta)
-                    (prepend mesa)))))
+                    (prepend mesa libva)))
+    (arguments
+     (substitute-keyword-arguments (package-arguments google-chrome-beta)
+       ((#:phases phases)
+        `(modify-phases ,phases
+           (add-after 'install-wrapper 'add-gpu-wrapper
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (use-modules (ice-9 rdelim)
+                            (ice-9 match))
+               (let* ((out (assoc-ref outputs "out"))
+                      (wrapper (string-append out "/bin/google-chrome-beta"))
+                      (mesa (assoc-ref inputs "mesa"))
+                      (libva (assoc-ref inputs "libva")))
+                 ;; Move the original wrapper
+                 (rename-file wrapper (string-append wrapper "-original"))
+                 ;; Create new wrapper with GPU support
+                 (with-output-to-file wrapper
+                   (lambda ()
+                     (format #t "#!/bin/sh~%")
+                     ;; Set GPU environment variables
+                     (format #t "export LIBGL_DRIVERS_PATH=\"~a:${LIBGL_DRIVERS_PATH}\"~%"
+                             (string-append mesa "/lib/dri"))
+                     (format #t "export MESA_LOADER_DRIVER_OVERRIDE=iris~%")
+                     (format #t "export LIBVA_DRIVERS_PATH=\"~a:${LIBVA_DRIVERS_PATH}\"~%"
+                             (string-append libva "/lib/dri"))
+                     (format #t "export LIBVA_DRIVER_NAME=iHD~%")
+                     (format #t "~%")
+                     ;; Execute the original wrapper with GPU flags
+                     (format #t "exec \"~a\" \\~%" (string-append wrapper "-original"))
+                     (format #t "    --use-gl=angle \\~%")
+                     (format #t "    --use-angle=gl \\~%")
+                     (format #t "    --enable-features=VaapiVideoDecoder,VaapiVideoEncoder,CanvasOopRasterization,Vulkan \\~%")
+                     (format #t "    --enable-gpu-rasterization \\~%")
+                     (format #t "    --enable-zero-copy \\~%")
+                     (format #t "    --disable-gpu-driver-bug-workarounds \\~%")
+                     (format #t "    --ignore-gpu-blocklist \\~%")
+                     (format #t "    \"$@\"~%")))
+                 (chmod wrapper #o755)
+                 #t)))))))))
+
+google-chrome-beta-system-egl
