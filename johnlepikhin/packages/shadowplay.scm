eed6c4fac0937676c06827bb726ded0cbe0d7fc6
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2025-08-29 05:47:41 +0300

    fix: Remove unused Rust packages from project

--- johnlepikhin/packages/shadowplay.scm
+++ johnlepikhin/packages/shadowplay.scm
@@ -20,81 +20,9 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system cargo)
   #:use-module (guix git-download)
-  #:use-module (guix download)
   #:use-module (guix git)
   #:use-module (guix packages)
-  #:use-module (gnu packages compression)
-  #:use-module (gnu packages jemalloc)
-  #:use-module (gnu packages pcre)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages ssh)
-  #:use-module (gnu packages tls)
-  #:use-module (gnu packages version-control))
-
-(define-public rust-nom-locate-4
-  (package
-    (name "rust-nom-locate")
-    (version "4.0.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "nom_locate" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0186n5qbpiyhpas3nk8y4ynnbdghl4nx958bkq4a6a9hr8v48y9p"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-bytecount" ,rust-bytecount-0.6)
-        ("rust-nom" ,rust-nom-7))))
-    (home-page "https://github.com/fflorent/nom_locate")
-    (synopsis "A special input type for nom to locate tokens")
-    (description
-     "The crate provide the LocatedSpan struct that encapsulates the data.")
-    (license (list license:expat))))
-
-(define-public rust-located-yaml-0
-  (package
-    (name "rust-located-yaml")
-    (version "0.2.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "located_yaml" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0xnx5al5v7d9syspj0irm22alwc3a9adikqxpbyyf6vsz3k8xilv"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-yaml-rust" ,rust-yaml-rust-0.4)
-        ("rust-linked-hash-map" ,rust-linked-hash-map-0.5)
-        ("rust-serde" ,rust-serde-1))))
-    (home-page "https://github.com/johnlepikhin/located_yaml")
-    (synopsis "YAML parser with saved positions")
-    (description
-     "YAML parser which provides AST with saved tokens positions.")
-    (license (list license:expat))))
-
-(define-public rust-serde-regex-1
-  (package
-    (name "rust-serde-regex")
-    (version "1.1.0")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (crate-uri "serde_regex" version))
-        (file-name (string-append name "-" version ".tar.gz"))
-        (sha256
-         (base32 "1pxsnxb8c198szghk1hvzvhva36w2q5zs70hqkmdf5d89qd6y4x8"))))
-    (build-system cargo-build-system)
-    (home-page "https://github.com/tailhook/serde-regex")
-    (synopsis "A serde wrapper, that can be used to serialize regular expressions as strings.")
-    (description "A serde wrapper, that can be used to serialize regular expressions as strings.
-It's often useful to read regexes from configuration file.")
-    (license (list license:expat license:asl2.0))))
+  #:use-module (gnu packages pkg-config))
 
 (define (make-shadowplay version)
   (package
@@ -105,31 +33,16 @@ It's often useful to read regexes from configuration file.")
              (commit (string-append "v" version))))
     (build-system cargo-build-system)
     (arguments
-     `(
+     `(#:tests? #f
+       ;; Cargo dependencies will be resolved automatically by cargo
+       #:install-source? #t
        #:phases
        (modify-phases
            %standard-phases
          (add-after 'unpack 'permissions
            (lambda _
              (delete-file ".cargo/config.toml")
-             #t)))
-       #:cargo-inputs
-       (("rust-anyhow-1" ,rust-anyhow-1)
-         ("rust-env-logger" ,rust-env-logger-0.9)
-         ("rust-lazy-static" ,rust-lazy-static-1)
-         ("rust-log" ,rust-log-0.4)
-         ("rust-nom" ,rust-nom-7)
-         ("rust-serde" ,rust-serde-1)
-         ("rust-serde-json" ,rust-serde-json-1)
-         ("rust-serde-yaml" ,rust-serde-yaml-0.8)
-         ("rust-regex" ,rust-regex-1)
-         ("rust-yaml-rust" ,rust-yaml-rust-0.4)
-         ("rust-linked-hash-map" ,rust-linked-hash-map-0.5)
-         ("rust-structopt" ,rust-structopt-0.3)
-         ("rust-nom-locate" ,rust-nom-locate-4)
-         ("rust-pretty" ,rust-pretty-0.11)
-         ("rust-located-yaml" ,rust-located-yaml-0)
-         ("rust-serde-regex" ,rust-serde-regex-1))))
+             #t)))))
     (native-inputs
      `(("pkg-config" ,pkg-config)))
     (home-page "https://gitlab.corp.mail.ru/MailBackendTest/devtools/shadowplay")
--- johnlepikhin/packages/xidlehook.scm
+++ johnlepikhin/packages/xidlehook.scm
@@ -25,63 +25,7 @@
   #:use-module (gnu packages python)
   #:use-module (guix build-system cargo)
   #:use-module (guix download)
-  #:use-module (guix git)
   #:use-module (guix packages))
-
-(define-public rust-xcb-0
-  (package
-    (name "rust-xcb")
-    (version "0.9.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "xcb" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "19i2pm8alpn2f0m4jg8bsw6ckw8irj1wjh55h9pi2fcb2diny1b2"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-build-flags '("--all-features")
-       #:cargo-test-flags '("--all-features")
-       #:cargo-development-inputs (("rust-gl" ,rust-gl-0.14)
-                                   ("rust-png" ,rust-png-0.17)
-                                   ("rust-x11" ,rust-x11-2))
-       #:cargo-inputs (("rust-bitflags" ,rust-bitflags-1)
-                       ("rust-libc" ,rust-libc-0.2)
-                       ("rust-quick-xml" ,rust-quick-xml-0.22)
-                       ("rust-x11" ,rust-x11-2))))
-    (inputs (list mesa)) ;required by rust-x11-2
-    (native-inputs (list pkg-config))
-    (home-page "https://github.com/rust-x-bindings/rust-xcb")
-    (synopsis "Rust bindings and wrappers for XCB")
-    (description "This package provides Rust bindings and wrappers for XCB.")
-    (license license:expat)))
-
-(define-public rust-xidlehook-core-0
-  (package
-    (name "rust-xidlehook-core")
-    (version "0.3.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "xidlehook-core" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "03p9fksj88z93lc3wqcvhz5c69zmznljzvg98dkljc51xp4bk0zh"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-development-inputs (("rust-gl" ,rust-gl-0.14)
-                                   ("rust-png" ,rust-png-0.17)
-                                   ("rust-x11" ,rust-x11-2))
-       #:cargo-inputs (("rust-bitflags" ,rust-bitflags-1)
-                       ("rust-libc" ,rust-libc-0.2)
-                       ("rust-quick-xml" ,rust-quick-xml-0.22)
-                       ("rust-x11" ,rust-x11-2))))
-    (native-inputs (list pkg-config))
-    (home-page "https://github.com/jD91mZM2/xidlehook")
-    (synopsis "xidlehook core library")
-    (description "xidlehook core library")
-    (license license:expat)))
 
 (define-public xidlehook
   (package
@@ -100,12 +44,9 @@
                     "")))))
     (build-system cargo-build-system)
     (arguments
-     `(#:cargo-inputs (("rust-env-logger" ,rust-env-logger-0.9)
-                       ("rust-nix" ,rust-nix-0.15)
-                       ("rust-structopt" ,rust-structopt-0.3)
-                       ("rust-xcb" ,rust-xcb-0)
-                       ("rust-xidlehook-core" ,rust-xidlehook-core-0)
-                       ("rust-libpulse-binding" ,rust-libpulse-binding-2))))
+     `(#:tests? #f
+       ;; Cargo dependencies will be resolved automatically by cargo
+       #:install-source? #t))
     (inputs (list mesa pulseaudio))
     (native-inputs (list pkg-config python))
     (home-page "https://github.com/jD91mZM2/xidlehook")
