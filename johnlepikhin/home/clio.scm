4184abb981f15ae5ffae45cde8cccf8ade09da85
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2026-02-23 21:13:28 +0300

    feat: Add passmenu-clio script to clio home service
    Generate a passmenu-clio script that uses dmenu to select a password
    from pass store and copies it to clipboard via clio with a configurable
    TTL and masked display. The script is installed to ~/.local/bin/ and
    can be disabled via the passmenu? configuration field.

--- johnlepikhin/home/clio.scm
+++ johnlepikhin/home/clio.scm
@@ -19,8 +19,12 @@
 (define-module (johnlepikhin home clio)
   #:use-module (gnu services)
   #:use-module (gnu home services)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages suckless)
   #:use-module (johnlepikhin home xsession)
   #:use-module (johnlepikhin packages clio)
+  #:use-module (guix gexp)
   #:use-module (guix records)
   #:export (home-clio-configuration
             home-clio-service-type))
@@ -28,7 +32,9 @@
 (define-record-type* <home-clio-configuration>
   home-clio-configuration make-home-clio-configuration
   home-clio-configuration?
-  (package home-clio-configuration-package (default clio)))
+  (package home-clio-configuration-package (default clio))
+  (passmenu? home-clio-configuration-passmenu? (default #t))
+  (passmenu-ttl home-clio-configuration-passmenu-ttl (default "30s")))
 
 (define (add-clio-package config)
   (list (home-clio-configuration-package config)))
@@ -36,12 +42,52 @@
 (define (add-xsession-component config)
   "clio watch &")
 
+(define (make-passmenu-clio-script config)
+  (let ((clio-pkg (home-clio-configuration-package config))
+        (ttl (home-clio-configuration-passmenu-ttl config)))
+    (computed-file
+     "passmenu-clio"
+     (gexp
+      (begin
+        (with-output-to-file
+            #$output
+          (lambda _
+            (display
+             (string-append
+              "#!" #$(file-append bash-minimal "/bin/bash") "\n"
+              "\n"
+              "shopt -s nullglob globstar\n"
+              "\n"
+              "dmenu=" #$(file-append dmenu "/bin/dmenu") "\n"
+              "pass=" #$(file-append password-store "/bin/pass") "\n"
+              "clio=" #$(file-append clio-pkg "/bin/clio") "\n"
+              "\n"
+              "prefix=${PASSWORD_STORE_DIR-~/.password-store}\n"
+              "password_files=( \"$prefix\"/**/*.gpg )\n"
+              "password_files=( \"${password_files[@]#\"$prefix\"/}\" )\n"
+              "password_files=( \"${password_files[@]%.gpg}\" )\n"
+              "\n"
+              "password=$(printf '%s\\n' \"${password_files[@]}\" | \"$dmenu\" \"$@\")\n"
+              "\n"
+              "[[ -n $password ]] || exit\n"
+              "\n"
+              "\"$pass\" show \"$password\" | head -1 | "
+              "\"$clio\" copy --ttl " #$ttl " --mask-with 'Secret from pass'\n"))))
+        (chmod #$output #o755))))))
+
+(define (add-passmenu-clio-file config)
+  (if (home-clio-configuration-passmenu? config)
+      `((".local/bin/passmenu-clio"
+         ,(make-passmenu-clio-script config)))
+      '()))
+
 (define home-clio-service-type
   (service-type
    (name 'home-clio)
    (extensions
     (list
      (service-extension home-profile-service-type add-clio-package)
-     (service-extension home-xsession-service-type add-xsession-component)))
+     (service-extension home-xsession-service-type add-xsession-component)
+     (service-extension home-files-service-type add-passmenu-clio-file)))
    (default-value (home-clio-configuration))
    (description "Install clio and start clipboard watcher in xsession")))
